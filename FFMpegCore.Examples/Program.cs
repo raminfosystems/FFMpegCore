8ed08cf6c1feba9f21f69cb7b4215d28e1c5b7ef
Ravi Lodhiya <ravi@raminfosystems.co.uk>
2022-09-20 15:09:36 +0100

    commit the changes

--- FFMpegCore.Examples/Program.cs
+++ FFMpegCore.Examples/Program.cs
@@ -7,50 +7,50 @@ using FFMpegCore.Enums;
 using FFMpegCore.Pipes;
 using FFMpegCore.Extend;
 
-var inputPath = "/path/to/input";
-var outputPath = "/path/to/output";
+//var inputPath = "/path/to/input";
+//var outputPath = "/path/to/output";
 
-{
-    var mediaInfo = FFProbe.Analyse(inputPath);
-}
+//{
+//    var mediaInfo = FFProbe.Analyse(inputPath);
+//}
 
-{
-    var mediaInfo = await FFProbe.AnalyseAsync(inputPath);
-}
+//{
+//    var mediaInfo = await FFProbe.AnalyseAsync(inputPath);
+//}
 
-{
-    FFMpegArguments
-        .FromFileInput(inputPath)
-        .OutputToFile(outputPath, false, options => options
-            .WithVideoCodec(VideoCodec.LibX264)
-            .WithConstantRateFactor(21)
-            .WithAudioCodec(AudioCodec.Aac)
-            .WithVariableBitrate(4)
-            .WithVideoFilters(filterOptions => filterOptions
-                .Scale(VideoSize.Hd))
-            .WithFastStart())
-        .ProcessSynchronously();
-}
+//{
+//    FFMpegArguments
+//        .FromFileInput(inputPath)
+//        .OutputToFile(outputPath, false, options => options
+//            .WithVideoCodec(VideoCodec.LibX264)
+//            .WithConstantRateFactor(21)
+//            .WithAudioCodec(AudioCodec.Aac)
+//            .WithVariableBitrate(4)
+//            .WithVideoFilters(filterOptions => filterOptions
+//                .Scale(VideoSize.Hd))
+//            .WithFastStart())
+//        .ProcessSynchronously();
+//}
 
-{
-    // process the snapshot in-memory and use the Bitmap directly
-    var bitmap = FFMpeg.Snapshot(inputPath, new Size(200, 400), TimeSpan.FromMinutes(1));
+//{
+//    // process the snapshot in-memory and use the Bitmap directly
+//    var bitmap = FFMpeg.Snapshot(inputPath, new Size(200, 400), TimeSpan.FromMinutes(1));
 
-    // or persists the image on the drive
-    FFMpeg.Snapshot(inputPath, outputPath, new Size(200, 400), TimeSpan.FromMinutes(1));
-}
+//    // or persists the image on the drive
+//    FFMpeg.Snapshot(inputPath, outputPath, new Size(200, 400), TimeSpan.FromMinutes(1));
+//}
 
-var inputStream = new MemoryStream();
-var outputStream = new MemoryStream();
+//var inputStream = new MemoryStream();
+//var outputStream = new MemoryStream();
 
-{
-    await FFMpegArguments
-        .FromPipeInput(new StreamPipeSource(inputStream))
-        .OutputToPipe(new StreamPipeSink(outputStream), options => options
-            .WithVideoCodec("vp9")
-            .ForceFormat("webm"))
-        .ProcessAsynchronously();
-}
+//{
+//    await FFMpegArguments
+//        .FromPipeInput(new StreamPipeSource(inputStream))
+//        .OutputToPipe(new StreamPipeSink(outputStream), options => options
+//            .WithVideoCodec("vp9")
+//            .ForceFormat("webm"))
+//        .ProcessAsynchronously();
+//}
 
 {
     FFMpeg.Join(@"..\joined_video.mp4",
@@ -60,75 +60,75 @@ var outputStream = new MemoryStream();
     );
 }
 
-{
-    FFMpeg.JoinImageSequence(@"..\joined_video.mp4", frameRate: 1,
-        ImageInfo.FromPath(@"..\1.png"),
-        ImageInfo.FromPath(@"..\2.png"),
-        ImageInfo.FromPath(@"..\3.png")
-    );
-}
+//{
+//    FFMpeg.JoinImageSequence(@"..\joined_video.mp4", frameRate: 1,
+//        ImageInfo.FromPath(@"..\1.png"),
+//        ImageInfo.FromPath(@"..\2.png"),
+//        ImageInfo.FromPath(@"..\3.png")
+//    );
+//}
 
-{
-    FFMpeg.Mute(inputPath, outputPath);
-}
+//{
+//    FFMpeg.Mute(inputPath, outputPath);
+//}
 
-{
-    FFMpeg.ExtractAudio(inputPath, outputPath);
-}
+//{
+//    FFMpeg.ExtractAudio(inputPath, outputPath);
+//}
 
-var inputAudioPath = "/path/to/input/audio";
-{
-    FFMpeg.ReplaceAudio(inputPath, inputAudioPath, outputPath);
-}
+//var inputAudioPath = "/path/to/input/audio";
+//{
+//    FFMpeg.ReplaceAudio(inputPath, inputAudioPath, outputPath);
+//}
 
-var inputImagePath = "/path/to/input/image";
-{
-    FFMpeg.PosterWithAudio(inputPath, inputAudioPath, outputPath);
-    // or
-    var image = Image.FromFile(inputImagePath);
-    image.AddAudio(inputAudioPath, outputPath);
-}
+//var inputImagePath = "/path/to/input/image";
+//{
+//    FFMpeg.PosterWithAudio(inputPath, inputAudioPath, outputPath);
+//    // or
+//    var image = Image.FromFile(inputImagePath);
+//    image.AddAudio(inputAudioPath, outputPath);
+//}
 
-IVideoFrame GetNextFrame() => throw new NotImplementedException();
-{
-    IEnumerable<IVideoFrame> CreateFrames(int count)
-    {
-        for(int i = 0; i < count; i++)
-        {
-            yield return GetNextFrame(); //method of generating new frames
-        }
-    }
+//IVideoFrame GetNextFrame() => throw new NotImplementedException();
+//{
+//    IEnumerable<IVideoFrame> CreateFrames(int count)
+//    {
+//        for(int i = 0; i < count; i++)
+//        {
+//            yield return GetNextFrame(); //method of generating new frames
+//        }
+//    }
 
-    var videoFramesSource = new RawVideoPipeSource(CreateFrames(64)) //pass IEnumerable<IVideoFrame> or IEnumerator<IVideoFrame> to constructor of RawVideoPipeSource
-    {
-        FrameRate = 30 //set source frame rate
-    };
-    await FFMpegArguments
-        .FromPipeInput(videoFramesSource)
-        .OutputToFile(outputPath, false, options => options
-            .WithVideoCodec(VideoCodec.LibVpx))
-        .ProcessAsynchronously();
-}
+//    var videoFramesSource = new RawVideoPipeSource(CreateFrames(64)) //pass IEnumerable<IVideoFrame> or IEnumerator<IVideoFrame> to constructor of RawVideoPipeSource
+//    {
+//        FrameRate = 30 //set source frame rate
+//    };
+//    await FFMpegArguments
+//        .FromPipeInput(videoFramesSource)
+//        .OutputToFile(outputPath, false, options => options
+//            .WithVideoCodec(VideoCodec.LibVpx))
+//        .ProcessAsynchronously();
+//}
 
-{
-    // setting global options
-    GlobalFFOptions.Configure(new FFOptions { BinaryFolder = "./bin", TemporaryFilesFolder = "/tmp" });
-    // or
-    GlobalFFOptions.Configure(options => options.BinaryFolder = "./bin");
+//{
+//    // setting global options
+//    GlobalFFOptions.Configure(new FFOptions { BinaryFolder = "./bin", TemporaryFilesFolder = "/tmp" });
+//    // or
+//    GlobalFFOptions.Configure(options => options.BinaryFolder = "./bin");
 
-    // or individual, per-run options
-    await FFMpegArguments
-        .FromFileInput(inputPath)
-        .OutputToFile(outputPath)
-        .ProcessAsynchronously(true, new FFOptions { BinaryFolder = "./bin", TemporaryFilesFolder = "/tmp" });
+//    // or individual, per-run options
+//    await FFMpegArguments
+//        .FromFileInput(inputPath)
+//        .OutputToFile(outputPath)
+//        .ProcessAsynchronously(true, new FFOptions { BinaryFolder = "./bin", TemporaryFilesFolder = "/tmp" });
 
-    // or combined, setting global defaults and adapting per-run options
-    GlobalFFOptions.Configure(new FFOptions { BinaryFolder = "./bin", TemporaryFilesFolder = "./globalTmp", WorkingDirectory = "./" });
+//    // or combined, setting global defaults and adapting per-run options
+//    GlobalFFOptions.Configure(new FFOptions { BinaryFolder = "./bin", TemporaryFilesFolder = "./globalTmp", WorkingDirectory = "./" });
 
-    await FFMpegArguments
-        .FromFileInput(inputPath)
-        .OutputToFile(outputPath)
-        .Configure(options => options.WorkingDirectory = "./CurrentRunWorkingDir")
-        .Configure(options => options.TemporaryFilesFolder = "./CurrentRunTmpFolder")
-        .ProcessAsynchronously();
-}
+//    await FFMpegArguments
+//        .FromFileInput(inputPath)
+//        .OutputToFile(outputPath)
+//        .Configure(options => options.WorkingDirectory = "./CurrentRunWorkingDir")
+//        .Configure(options => options.TemporaryFilesFolder = "./CurrentRunTmpFolder")
+//        .ProcessAsynchronously();
+//}
